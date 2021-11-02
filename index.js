404599c5e3f939664e3a012cd76cbb52db2ff2e9
JazzPants <leighton.l14@gmail.com>
2021-11-02 18:31:34 +1100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+const currentUser = "Grace Hopper";
+
+console.log(currentUser);
+
+const welcomeMessage = `Welcome to Flatbook,  ${currentUser}!`; //template literal interpolation
+//
+
+
+console.log(welcomeMessage);
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+console.log(excitedWelcomeMessage);
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`; // ${currentUser[0]} also works
+
+console.log(shortGreeting);
